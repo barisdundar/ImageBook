67949198e91ccf64f801e3573ee67f82f167e99b
Şerafettin Barış Dundar <baris_809@windowslive.com>
2022-11-04 17:12:31 +0300

    Image Book

---
+++ ImageBook/app/src/main/java/com/barisdundar/imagebook/Art.java
@@ -0,0 +1,11 @@
+package com.barisdundar.imagebook;
+
+public class Art {
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
